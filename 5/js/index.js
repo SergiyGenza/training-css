5479a3b93cce64d1191ffdb3884e4b9dea06416f
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2023-12-21 22:30:00 +0200

    Fix: grid template and footer position

--- 5/js/index.js
+++ 5/js/index.js
@@ -2,7 +2,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const id = entry.target.getAttribute("id");
-      console.log(id);
       if (entry.intersectionRatio > 0) {
         document
           .querySelector(`div ul li a[href="#${id}"]`)
