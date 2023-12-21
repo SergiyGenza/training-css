e53c2d141147d51f7ec41cd58f793c62ce0fdcb5
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2023-12-21 21:50:16 +0200

    add sidebar with navigation

--- 5/js/index.js
+++ 5/js/index.js
@@ -1,1 +1,22 @@
-// console.log('hello');
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      console.log(id);
+      if (entry.intersectionRatio > 0) {
+        document
+          .querySelector(`div ul li a[href="#${id}"]`)
+          .parentElement.classList.add("active");
+      } else {
+        document
+          .querySelector(`div ul li a[href="#${id}"]`)
+          .parentElement.classList.remove("active");
+      }
+    });
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("div[id]").forEach((section) => {
+    observer.observe(section);
+  });
+});
